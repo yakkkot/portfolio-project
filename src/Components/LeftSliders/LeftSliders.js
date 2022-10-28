4584bd4bd1375b5da85ffd94a94953af0d4a149e
yakkkot <k0tjeee777@gmail.com>
2022-10-28 18:44:36 +0300

    working with media - second

--- src/Components/LeftSliders/LeftSliders.js
+++ src/Components/LeftSliders/LeftSliders.js
@@ -7,22 +7,22 @@ const LeftSliders = () => {
     return (
         <div className='hidden lg:flex fixed flex-col top-[34%] left-0 z-20'>
             <ul className='container_links_nav'>
-                <li className="small_container px-2 w-[160px] h-[60px] left_menu_container bg-blue-600 flex items-center gap-4 text-2xl">
+                <li className="small_container px-2 w-[150px] h-[60px] left_menu_container bg-blue-600 flex items-center gap-4 text-[20px]">
                     <a href="https://www.linkedin.com/in/yakkkot" target="_blank" className='flex items-center justify-between w-full'>LinkedIn
                         <FaLinkedin color={'white'} size={30}/>
                     </a>
                 </li>
-                <li className="small_container px-2 w-[160px] h-[60px] left_menu_container bg-gray-700 flex items-center gap-4 text-2xl">
+                <li className="small_container px-2 w-[150px] h-[60px] left_menu_container bg-gray-700 flex items-center gap-4 text-[20px]">
                     <a href="https://github.com/yakkkot" target="_blank" className='flex items-center justify-between w-full'>GitHub
                         <FaGithub color={'white'} size={30}/>
                     </a>
                 </li>
-                <li className="small_container px-2 w-[160px] h-[60px] left_menu_container bg-blue-500 flex items-center gap-4 text-2xl">
+                <li className="small_container px-2 w-[150px] h-[60px] left_menu_container bg-blue-500 flex items-center gap-4 text-[20px]">
                     <a href="https://t.me/yakkkot" target="_blank" className='flex items-center justify-between w-full'>Telegram
                         <BsTelegram color={'white'} size={30}/>
                     </a>
                 </li>
-                <li className="small_container px-2 w-[160px] h-[60px] left_menu_container bg-violet-700 flex items-center gap-4 text-2xl">
+                <li className="small_container px-2 w-[150px] h-[60px] left_menu_container bg-violet-700 flex items-center gap-4 text-[20px]">
                     <a href="https://resume.io/r/DpMLvp5CE" target="_blank" className='flex items-center justify-between w-full'>Resume
                         <BsFillPersonLinesFill color={'white'} size={30}/>
                     </a>
